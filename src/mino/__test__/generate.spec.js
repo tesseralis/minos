66160016dc5db053639f16595dd6b6bdd1e01051
Nat Alison <tesseralis@gmail.com>
2019-04-10 21:46:27 -0700

    test the rest

--- src/mino/__test__/generate.spec.js
+++ src/mino/__test__/generate.spec.js
@@ -32,4 +32,12 @@ describe('generateGraph', () => {
     const sizes = [0, ...nodes.map(gen => gen.length)]
     expect(sizes).toEqual(freeCounts)
   })
+
+  it('does not generate children for the last generation', () => {
+    const { nodes, meta } = generateGraph(8)
+    const lastGen = nodes[nodes.length - 1]
+    for (let mino of lastGen) {
+      expect([...meta[mino].children]).toHaveLength(0)
+    }
+  })
 })
