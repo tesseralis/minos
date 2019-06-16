273228a022dcfb9c50ffaabefe416245fab6c544
Nat Alison <tesseralis@gmail.com>
2019-06-16 00:14:15 -0700

    fix fast zooming on firefox

--- src/app/PanZoom.jsx
+++ src/app/PanZoom.jsx
@@ -1,6 +1,16 @@
 import React, { useRef, useEffect } from 'react'
 import panzoom from 'panzoom'
 
+// Panzoom has a bug where scrolling (on Mac) is a lot faster on Firefox than on
+// Chrome or Safari. Offset this by making the scrollspeed slower if Firefox.
+function getNormalizedSpeed(speed) {
+  // Detect Firefox: https://stackoverflow.com/a/9851769
+  if (typeof InstallTrigger !== 'undefined') {
+    return speed / 3
+  }
+  return speed
+}
+
 // TODO styles on panning
 export default function PanZoom({ children, zoomSpeed, minZoom, maxZoom }) {
   const group = useRef(null)
@@ -8,7 +18,7 @@ export default function PanZoom({ children, zoomSpeed, minZoom, maxZoom }) {
   useEffect(() => {
     const instance = panzoom(group.current, {
       smoothScroll: false,
-      zoomSpeed,
+      zoomSpeed: getNormalizedSpeed(zoomSpeed),
       maxZoom,
       minZoom,
     })
