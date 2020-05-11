e0a04456995c402fd09c5e8ad2fd74eb6d5bcd46
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-05-11 01:05:32 -0700

    rename

---
+++ src/app/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react"
+import { css } from "emotion"
+
+import type { Mino } from "mino/mino"
+
+import InfoButton from "./InfoButton"
+import Compass from "./Compass"
+import MinoGraph from "./MinoGraph"
+
+export default function App() {
+  const [selected, setSelected] = useState<Mino | undefined>()
+  // const [hovered, setHovered] = useState<Mino | undefined>()
+
+  return (
+    <div
+      className={css`
+        display: grid;
+
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+      `}
+    >
+      <div
+        className={css`
+          grid-area: 1 / 1;
+        `}
+      >
+        <MinoGraph selected={selected} onSelect={setSelected} />
+      </div>
+      <div
+        className={css`
+          grid-area: 1 / 1;
+          align-self: end;
+          justify-self: start;
+          padding: 2rem;
+        `}
+      >
+        <InfoButton />
+      </div>
+      {selected && (
+        <div
+          key={selected}
+          className={css`
+            grid-area: 1 / 1;
+            align-self: start;
+            justify-self: end;
+            padding: 2rem;
+            pointer-events: none;
+          `}
+        >
+          <Compass mino={selected} onSelect={setSelected} />
+        </div>
+      )}
+    </div>
+  )
+}
