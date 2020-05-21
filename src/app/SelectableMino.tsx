1930db753aa60ed313ec37ff819d02080be1553f
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-05-21 02:40:47 -0700

    selectableMino lint

--- src/app/SelectableMino.tsx
+++ src/app/SelectableMino.tsx
@@ -20,57 +20,53 @@ interface Props {
   onHover?(mino?: Mino): void
 }
 
-const SelectableMino = memo(
-  ({
-    mino,
-    cx,
-    cy,
-    size,
-    fill,
-    stroke,
-    selected = false,
-    onSelect,
-    onHover,
-  }: Props) => {
-    const [hovered, setHovered] = useState(false)
+export default memo(function SelectableMino({
+  mino,
+  cx,
+  cy,
+  size,
+  fill,
+  stroke,
+  selected = false,
+  onSelect,
+  onHover,
+}: Props) {
+  const [hovered, setHovered] = useState(false)
 
-    const n = getSize(mino)
-    const onClick = useCallback(() => onSelect?.(mino), [mino, onSelect])
-    const handleHover = useCallback(
-      (value) => {
-        setHovered(value)
-        onHover?.(value ? mino : undefined)
-      },
-      [mino, onHover],
-    )
-    const handleClick = useClickHandler(onClick)
+  const n = getSize(mino)
+  const onClick = useCallback(() => onSelect?.(mino), [mino, onSelect])
+  const handleHover = useCallback(
+    (value) => {
+      setHovered(value)
+      onHover?.(value ? mino : undefined)
+    },
+    [mino, onHover],
+  )
+  const handleClick = useClickHandler(onClick)
 
-    return (
-      <g>
-        <MinoSvg
-          mino={mino}
-          cx={cx}
-          cy={cy}
-          size={size * (hovered ? 1.25 : 1)}
-          fill={fill}
-          stroke={selected ? colors.fg : stroke}
-        />
-        <circle
-          {...handleClick}
-          className={css`
-            opacity: 0;
-            cursor: pointer;
-          `}
-          tabIndex={0}
-          cx={cx}
-          cy={cy}
-          r={(n * size) / 2}
-          onMouseOver={() => handleHover(true)}
-          onMouseOut={() => handleHover(false)}
-        />
-      </g>
-    )
-  },
-)
-
-export default SelectableMino
+  return (
+    <g>
+      <MinoSvg
+        mino={mino}
+        cx={cx}
+        cy={cy}
+        size={size * (hovered ? 1.25 : 1)}
+        fill={fill}
+        stroke={selected ? colors.fg : stroke}
+      />
+      <circle
+        {...handleClick}
+        className={css`
+          opacity: 0;
+          cursor: pointer;
+        `}
+        tabIndex={0}
+        cx={cx}
+        cy={cy}
+        r={(n * size) / 2}
+        onMouseOver={() => handleHover(true)}
+        onMouseOut={() => handleHover(false)}
+      />
+    </g>
+  )
+})
