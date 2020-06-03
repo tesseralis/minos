8daabbfbe7e930df13652b8c9e7dfdf4f9e473a7
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-06-02 23:37:40 -0700

    add react-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import { css } from "emotion"
 
 import { Mino } from "mino"
@@ -9,100 +10,106 @@ import Compass from "./Compass"
 import FamilyTree from "./FamilyTree"
 import MinoList from "./MinoList"
 
-const views = ["graph", "list"] as const
-type View = "graph" | "list"
-
 export default function App() {
   const [selected, setSelected] = useState<Mino | undefined>()
-  // TODO replace this with a router
-  const [view, setView] = useState<View>("list")
 
   return (
-    <div
-      className={css`
-        display: grid;
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        /* Needed to make the graph full-height in Safari */
-        width: 100%;
-        height: 100%;
-      `}
-    >
+    <BrowserRouter>
       <div
         className={css`
-          grid-area: 1 / 1;
+          display: grid;
+          position: fixed;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
           /* Needed to make the graph full-height in Safari */
-          display: flex;
+          width: 100%;
+          height: 100%;
         `}
       >
-        {view === "graph" && (
-          <FamilyTree
-            selected={selected && getCanonical(selected)}
-            onSelect={setSelected}
-          />
-        )}
-        {view === "list" && (
-          <MinoList selected={selected} onSelect={setSelected} />
-        )}
-      </div>
-      <ul
-        className={css`
-          grid-area: 1/1;
-          align-self: start;
-          justify-self: start;
-          padding: 2rem;
-        `}
-      >
-        {views.map((view) => (
-          <li key={view}>
-            {/* TODO make this a link with a router */}
-            <button
-              className={css`
-                font-family: serif;
-                cursor: pointer;
-                background: none;
-                border: none;
-                color: white;
-                font-size: 1.5rem;
-                :hover {
-                  color: gray;
-                  text-decoration: underline;
-                }
-              `}
-              onClick={() => setView(view)}
-            >
-              {view}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div
-        className={css`
-          grid-area: 1 / 1;
-          align-self: end;
-          justify-self: start;
-          padding: 2rem;
-        `}
-      >
-        <InfoButton />
-      </div>
-      {selected && (
         <div
-          key={selected}
           className={css`
             grid-area: 1 / 1;
-            align-self: start;
-            justify-self: end;
-            padding: 2rem;
-            pointer-events: none;
+            /* Needed to make the graph full-height in Safari */
+            display: flex;
           `}
         >
-          <Compass mino={selected} onSelect={setSelected} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <FamilyTree
+                  selected={selected && getCanonical(selected)}
+                  onSelect={setSelected}
+                />
+              }
+            />
+            <Route
+              path="/list"
+              element={<MinoList selected={selected} onSelect={setSelected} />}
+            />
+          </Routes>
         </div>
-      )}
-    </div>
+        <nav
+          className={css`
+            grid-area: 1/1;
+            align-self: start;
+            justify-self: start;
+            padding: 2rem;
+          `}
+        >
+          <ul>
+            {["graph", "list"].map((view) => (
+              <li key={view}>
+                {/* TODO make this a link with a router */}
+                <NavLink
+                  to={view === "graph" ? "/" : `/${view}`}
+                  className={css`
+                    font-family: serif;
+                    color: #aaa;
+                    font-size: 1.5rem;
+                    text-decoration: none;
+                    :hover {
+                      text-decoration: underline;
+                    }
+                  `}
+                  end
+                  activeClassName={css`
+                    color: white;
+                  `}
+                >
+                  {view}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className={css`
+            grid-area: 1 / 1;
+            align-self: end;
+            justify-self: start;
+            padding: 2rem;
+          `}
+        >
+          <InfoButton />
+        </div>
+        {selected && (
+          <div
+            key={selected}
+            className={css`
+              grid-area: 1 / 1;
+              align-self: start;
+              justify-self: end;
+              padding: 2rem;
+              pointer-events: none;
+            `}
+          >
+            <Compass mino={selected} onSelect={setSelected} />
+          </div>
+        )}
+      </div>
+    </BrowserRouter>
   )
 }
