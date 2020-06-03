a92a6140f4616780cd73d4cb1846b616a5041dab
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-06-02 21:46:42 -0700

    make the arrangement more nice

--- src/app/MinoList.tsx
+++ src/app/MinoList.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { css } from "emotion"
 
+import { getSize, getShape, transform } from "mino"
 import { nodes, sortMinos, getMinoColor } from "./graph"
 
 import SelectableMino from "./SelectableMino"
@@ -20,28 +21,45 @@ export default function MinoList() {
         <section
           key={i}
           className={css`
+            display: flex;
+            flex-wrap: wrap;
             margin: 2rem;
           `}
         >
-          {sortMinos(minos).map((mino) => (
-            <span
-              key={mino}
-              className={css`
-                margin-right: 1rem;
-                margin-bottom: 1rem;
-              `}
-            >
-              <svg width={50} height={50}>
-                <SelectableMino
-                  coord={[0, 0]}
-                  mino={mino}
-                  size={10}
-                  anchor="top left"
-                  {...getMinoColor(mino)}
-                />
-              </svg>
-            </span>
-          ))}
+          {sortMinos(minos).map((mino) => {
+            mino = transform(mino, "rotateRight")
+            const [height, width] = getShape(mino)
+
+            const blockSize = 18 - getSize(mino)
+
+            const svgWidth = width * blockSize * 1.125
+            const svgHeight = height * blockSize * 1.125
+
+            return (
+              <div
+                key={mino}
+                className={css`
+                  margin-right: 1rem;
+                  margin-bottom: 1rem;
+                `}
+              >
+                <svg
+                  width={svgWidth}
+                  height={svgHeight}
+                  viewBox={`${-svgWidth / 2} ${
+                    -svgHeight / 2
+                  } ${svgWidth} ${svgHeight}`}
+                >
+                  <SelectableMino
+                    coord={[0, 0]}
+                    mino={mino}
+                    size={blockSize}
+                    {...getMinoColor(mino)}
+                  />
+                </svg>
+              </div>
+            )
+          })}
         </section>
       ))}
     </main>
