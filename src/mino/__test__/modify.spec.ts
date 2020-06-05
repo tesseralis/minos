7b24358c7500c5fec8860c8469ccff18fe4b2bba
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-06-05 01:30:58 -0700

    use fromString instead of fromBits

--- src/mino/__test__/modify.spec.ts
+++ src/mino/__test__/modify.spec.ts
@@ -1,43 +1,43 @@
-import { fromBits } from "../data"
+import { fromString } from "../data"
 import { addSquare, removeSquare } from "../modify"
 describe("modify", () => {
   describe("addSquare", () => {
     // L tetromino
-    const mino = fromBits(0b100_111, 3)
+    const mino = fromString("100_111")
     it("works correctly on an inner coordinate", () => {
-      expect(addSquare(mino, [1, 1])).toEqual(fromBits(0b110_111, 3))
+      expect(addSquare(mino, [1, 1])).toEqual(fromString("110_111"))
     })
 
     it("works correctly when i < 0", () => {
-      expect(addSquare(mino, [-1, 0])).toEqual(fromBits(0b100_111_001, 3))
+      expect(addSquare(mino, [-1, 0])).toEqual(fromString("100_111_001"))
     })
 
     it("works correctly when j < 0", () => {
-      expect(addSquare(mino, [0, -1])).toEqual(fromBits(0b1000_1111, 4))
+      expect(addSquare(mino, [0, -1])).toEqual(fromString("1000_1111"))
     })
 
     it("works correctly when j === width", () => {
-      expect(addSquare(mino, [0, 3])).toEqual(fromBits(0b0100_1111, 4))
+      expect(addSquare(mino, [0, 3])).toEqual(fromString("0100_1111"))
     })
   })
 
   describe("removeSquare", () => {
     // X pentomino
-    const mino = fromBits(0b010_111_010, 3)
+    const mino = fromString("010_111_010")
     it("works correctly when adjustments not needed", () => {
-      expect(removeSquare(mino, [2, 1])).toEqual(fromBits(0b111_010, 3))
+      expect(removeSquare(mino, [2, 1])).toEqual(fromString("111_010"))
     })
 
     it("works correctly when a downshift is needed", () => {
-      expect(removeSquare(mino, [0, 1])).toEqual(fromBits(0b010_111, 3))
+      expect(removeSquare(mino, [0, 1])).toEqual(fromString("010_111"))
     })
 
     it("works correctly when left shift is needed", () => {
-      expect(removeSquare(mino, [1, 0])).toEqual(fromBits(0b01_11_01, 2))
+      expect(removeSquare(mino, [1, 0])).toEqual(fromString("01_11_01"))
     })
 
     it("works correctly when decreasing width is needed", () => {
-      expect(removeSquare(mino, [1, 2])).toEqual(fromBits(0b10_11_10, 2))
+      expect(removeSquare(mino, [1, 2])).toEqual(fromString("10_11_10"))
     })
   })
 })
