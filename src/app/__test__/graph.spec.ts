6db0ce80e54738ca5c397fdefe64ab4d57721065
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-03-12 22:47:00 -0800

    add smoke test for tilings

--- src/app/__test__/graph.spec.ts
+++ src/app/__test__/graph.spec.ts
@@ -1,17 +1,35 @@
 import { generateGraph } from "../graph"
+import { getTiling } from "mino/tiling"
 
-const freeCounts = [0, 1, 1, 2, 5, 12, 35, 108, 369]
+const freeCounts = [1, 1, 2, 5, 12, 35, 108, 369]
+const nontilerCounts = [0, 0, 0, 0, 0, 0, 4, 26]
 
-describe("generateGraph", () => {
+// FIXME rename this file
+describe("app mino logic", () => {
   let graph: ReturnType<typeof generateGraph>
-
   beforeAll(() => {
     graph = generateGraph(8)
   })
 
-  it("correctly generates the free minos", () => {
-    const { nodes } = graph
-    const sizes = [0, ...nodes.map((gen) => gen.length)]
-    expect(sizes).toEqual(freeCounts)
+  describe("generateGraph", () => {
+    it("correctly generates the free minos", () => {
+      const { nodes } = graph
+      const sizes = nodes.map((gen) => gen.length)
+      expect(sizes).toEqual(freeCounts)
+    })
+  })
+
+  describe("getTiling", () => {
+    it("returns the correct number of tiling minos", () => {
+      const { nodes } = graph
+      const nontilers = nodes.map((gen) =>
+        gen.filter((mino) => !getTiling(mino)),
+      )
+      for (const nontiler of nontilers[6]) {
+        console.log(nontiler.display())
+      }
+      const actualCounts = nontilers.map((gen) => gen.length)
+      expect(actualCounts).toEqual(nontilerCounts)
+    })
   })
 })
