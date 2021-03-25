f579fea6e9d345c744a480ff05954e267bd42b6b
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-03-24 21:33:19 -0700

    mockup of filter

--- src/app/ListPage/index.tsx
+++ src/app/ListPage/index.tsx
@@ -1,10 +1,100 @@
-import React from "react"
+import React, { useMemo, useState } from "react"
 import { css } from "@emotion/css"
 import { nodes } from "app/graph"
 import useWindowEventListener from "app/useWindowEventListener"
 
+import { Symmetry } from "mino"
 import MinoList from "app/MinoList"
 import { useSelected, useSetSelected } from "app/SelectedContext"
+import MinoSvg from "app/MinoSvg"
+import GenerationRings from "app/FamilyTree/GenerationRings"
+import { getTiling } from "mino/tiling"
+
+type YesNo = "yes" | "no"
+type Range = [min: number, max: number]
+
+interface MinoFilter {
+  symmetries?: Symmetry[]
+  // boolean properties
+  isConvex?: YesNo
+  hasHole?: YesNo
+  hasTiling?: YesNo
+  /** Minimum and maximum bounding boxes to allow */
+  minDimensions?: [number, number]
+  maxDimensions?: [number, number]
+  /** Range for number of parents to allow */
+  numParents?: Range
+  /** Range for number of children to allow */
+  numChildren?: Range
+}
+
+interface Props {
+  value: MinoFilter
+  onUpdate(value: MinoFilter): void
+}
+
+function YesNoOption({ name, value, onUpdate }: any) {
+  return (
+    <div>
+      {name}:
+      {[undefined, "yes", "no"].map((val) => {
+        return (
+          <label key={val}>
+            <input
+              type="radio"
+              name={name}
+              value={val}
+              checked={value === val}
+              onChange={(e) => onUpdate(e.target.value)}
+            />
+            {val ?? "Show all"}
+          </label>
+        )
+      })}
+    </div>
+  )
+}
+
+function Filter({ value, onUpdate }: Props) {
+  return (
+    <form>
+      <YesNoOption
+        name="isConvex"
+        onUpdate={(val: any) => onUpdate({ ...value, isConvex: val })}
+      />
+      <YesNoOption
+        name="hasHole"
+        onUpdate={(val: any) => onUpdate({ ...value, hasHole: val })}
+      />
+      <YesNoOption
+        name="hasTiling"
+        onUpdate={(val: any) => onUpdate({ ...value, hasTiling: val })}
+      />
+    </form>
+  )
+}
+
+function applyFilter({ isConvex, hasHole, hasTiling }: MinoFilter) {
+  return nodes.map((generation) => {
+    let filtered = generation
+    if (isConvex) {
+      filtered = filtered.filter((p) =>
+        isConvex === "yes" ? p.isConvex() : !p.isConvex(),
+      )
+    }
+    if (hasHole) {
+      filtered = filtered.filter((p) =>
+        hasHole === "yes" ? p.hasHole() : !p.hasHole(),
+      )
+    }
+    if (hasTiling) {
+      filtered = filtered.filter((p) =>
+        hasTiling === "yes" ? getTiling(p) : !getTiling(p),
+      )
+    }
+    return filtered
+  })
+}
 
 /**
  * Displays the list of all minos for each generation
@@ -12,6 +102,7 @@ import { useSelected, useSetSelected } from "app/SelectedContext"
 export default function ListPage() {
   const selected = useSelected()
   const setSelected = useSetSelected()
+  const [filter, setFilter] = useState<MinoFilter>({})
   useWindowEventListener("click", (e) => {
     // Deselect the current mino if the click target isn't a mino
     // or the compass
@@ -20,6 +111,8 @@ export default function ListPage() {
       setSelected(null)
     }
   })
+
+  const minos = useMemo(() => applyFilter(filter), [filter])
 
   return (
     <main
@@ -31,7 +124,8 @@ export default function ListPage() {
         overflow-y: scroll;
       `}
     >
-      <MinoList minos={nodes} selected={selected} onSelect={setSelected} />
+      <Filter value={filter} onUpdate={setFilter} />
+      <MinoList minos={minos} selected={selected} onSelect={setSelected} />
     </main>
   )
 }
