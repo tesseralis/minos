6d0a1779b892701be7ea00cb090a3f48f717e2fa
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-04-02 19:42:20 -0700

    adjustments

--- src/app/MinoList/MinoFilter/InputTitle.tsx
+++ src/app/MinoList/MinoFilter/InputTitle.tsx
@@ -2,16 +2,22 @@ import React from "react"
 import { css } from "@emotion/css"
 import { colors } from "style/theme"
 
-interface InputTitleProps {
+interface Props {
   display: string
   onClear(): void
 }
 
-export default function InputTitle({ display, onClear }: InputTitleProps) {
+/**
+ * The display for a group of inputs
+ */
+export default function InputTitle({ display, onClear }: Props) {
   return (
     <div
       className={css`
         font-size: 1.125rem;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 4rem;
       `}
     >
       {display}
@@ -19,6 +25,9 @@ export default function InputTitle({ display, onClear }: InputTitleProps) {
         type="button"
         onClick={onClear}
         className={css`
+          /* TODO (refactor) factor out this button style */
+          font-size: 1rem;
+          font-family: serif;
           margin-left: 1rem;
           color: ${colors.fg};
           background: none;
