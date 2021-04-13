42590e8f295ca2e8d92768b89699c2e0b6670d33
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-04-12 17:10:13 -0700

    remove clicking on pattern mino for now

--- src/app/PackingPage/PatternMino.tsx
+++ src/app/PackingPage/PatternMino.tsx
@@ -1,9 +1,7 @@
 import tinycolor from "tinycolor2"
 import React from "react"
-import { useSetSelected } from "app/SelectedContext"
 import MinoSvg from "app/MinoSvg"
 import { getMinoColor } from "app/graph"
-import { colors } from "style/theme"
 import { Polyomino } from "mino"
 import Vector from "vector"
 
@@ -18,10 +16,7 @@ export default React.memo(function PatternMino({
   blockSize,
   mino,
   coord,
-  isSelected,
 }: Props) {
-  const setSelected = useSetSelected()
-  const [hovered, setHovered] = React.useState(false)
   const { fill } = getMinoColor(mino)
   let baseFill = tinycolor(fill)
   baseFill =
@@ -34,17 +29,9 @@ export default React.memo(function PatternMino({
       coord={coord.scale(blockSize)}
       anchor="top left"
       size={blockSize}
-      fill={
-        isSelected
-          ? tinycolor.mix(baseFill, colors.highlight).toString()
-          : hovered
-          ? tinycolor.mix(baseFill, colors.highlight, 30).toString()
-          : baseFill.toString()
-      }
+      fill={baseFill.toString()}
       stroke="black"
       gridStyle="thin"
-      onClick={() => setSelected(mino)}
-      onHover={setHovered}
     />
   )
 })
