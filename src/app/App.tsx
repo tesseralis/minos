7514d636eac3479191402dea8ccda744f83642bf
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-04-15 23:23:09 -0700

    move out the layout and stuff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import KeyBindings from "./KeyBindings"
 import Nav from "./Nav"
 import Layout from "./Layout"
-import SelectedContext from "./SelectedContext"
-import Compass from "./Compass"
 import FamilyTree from "./FamilyTree"
 import CatalogPage from "./CatalogPage"
 import PatternPage from "./PackingPage"
@@ -14,27 +12,22 @@ import ClassesPage from "./ClassesPage"
 
 export default function App() {
   return (
-    <SelectedContext.Provider>
+    <BrowserRouter>
       <KeyBindings />
-      <BrowserRouter>
-        <Layout topLeft={<Nav />} topRight={<Compass />}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/genealogy" />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/catalog/:mino" element={<CatalogPage />} />
-            {/* TODO: put this logic in <PatternPage somehow/> */}
-            <Route
-              path="/packing"
-              element={<Navigate to="/packing/5/rect" />}
-            />
-            <Route path="/packing/:size/:shape" element={<PatternPage />} />
-            <Route path="/classes" element={<ClassesPage />} />
-            <Route path="/tiling" element={<TilingPage />} />
-            <Route path="/tiling/:mino" element={<TilingPage />} />
-            <Route path="/genealogy" element={<FamilyTree />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </SelectedContext.Provider>
+      <Layout topLeft={<Nav />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/genealogy" />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:mino" element={<CatalogPage />} />
+          {/* TODO: put this logic in <PatternPage somehow/> */}
+          <Route path="/packing" element={<Navigate to="/packing/5/rect" />} />
+          <Route path="/packing/:size/:shape" element={<PatternPage />} />
+          <Route path="/classes" element={<ClassesPage />} />
+          <Route path="/tiling" element={<TilingPage />} />
+          <Route path="/tiling/:mino" element={<TilingPage />} />
+          <Route path="/genealogy" element={<FamilyTree />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
--- src/app/FamilyTree/FamilyTree.tsx
+++ src/app/FamilyTree/FamilyTree.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { css } from "@emotion/css"
 
 import Background from "./Background"
 import PanZoom from "./PanZoom"
 import FullScreenSvg from "./FullScreenSvg"
 import GenerationRings from "./GenerationRings"
 import MinoLinks from "./MinoLinks"
+import SelectedContext from "app/SelectedContext"
+import Compass from "app/Compass"
 
 /**
  * A graph showing the "family tree" of minos,
@@ -12,17 +15,31 @@ import MinoLinks from "./MinoLinks"
  */
 export default function FamilyTree() {
   return (
-    <FullScreenSvg width={1100}>
-      <Background />
-      <PanZoom
-        minZoom={0.25}
-        maxZoom={2}
-        zoomSpeed={0.075}
-        smoothScroll={false}
-      >
-        <MinoLinks />
-        <GenerationRings />
-      </PanZoom>
-    </FullScreenSvg>
+    <SelectedContext.Provider>
+      <div>
+        <FullScreenSvg width={1100}>
+          <Background />
+          <PanZoom
+            minZoom={0.25}
+            maxZoom={2}
+            zoomSpeed={0.075}
+            smoothScroll={false}
+          >
+            <MinoLinks />
+            <GenerationRings />
+          </PanZoom>
+        </FullScreenSvg>
+        <div
+          className={css`
+            position: absolute;
+            top: 0;
+            right: 0;
+            pointer-events: none;
+          `}
+        >
+          <Compass />
+        </div>
+      </div>
+    </SelectedContext.Provider>
   )
 }
