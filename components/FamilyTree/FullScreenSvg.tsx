bda244e023be86160a4902bb8c0608d62f79bda8
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-04-20 22:59:23 -0700

    change the fixme

--- components/FamilyTree/FullScreenSvg.tsx
+++ components/FamilyTree/FullScreenSvg.tsx
@@ -11,8 +11,7 @@ interface Props {
 export default function FullScreenSvg({ width, children }: Props) {
   // Only change the viewbox if the prop width changes, not the window ratio
   const viewBox = useMemo(() => {
-    // const height = (width / window.innerWidth) * window.innerHeight
-    // FIXME figure out how to do this based on window
+    // TODO does setting this matter if the window size is different?
     const height = (width / 16) * 9
     return `-${width / 2} ${-height / 10} ${width} ${height}`
   }, [width])
