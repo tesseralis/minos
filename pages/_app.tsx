17506791c301280e37c93dc236bb5412fe05a67a
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-04-21 16:21:44 -0700

    laksjdflkj

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import "style/theme.css"
 import Layout from "components/Layout"
 import Nav from "components/Nav"
 
+// the default app
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout topLeft={<Nav />}>
