85af0aa27e0fca28f223d5c77d5b602cdc32a03d
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-05-15 21:39:25 -0700

    add symmetry pages

---
+++ pages/symmetry/[symmetry].page.tsx
@@ -0,0 +1,80 @@
+import { capitalize } from "lodash"
+import fs from "fs"
+import type { GetStaticProps } from "next"
+import Link from "next/link"
+import { serialize } from "next-mdx-remote/serialize"
+import { MDXRemote } from "next-mdx-remote"
+import { css } from "@emotion/react"
+import { printSymmetry, symmetries, Symmetry } from "mino"
+import Layout from "components/Layout"
+
+function SymmetryLinks() {
+  // FIXME is this the right markup for a nav header?
+  return (
+    <div>
+      <div>Symmetries</div>
+      <nav>
+        {symmetries.map((symmetry) => {
+          return (
+            <div key={symmetry}>
+              <Link href={`/symmetry/${symmetry}`}>
+                <a>{printSymmetry(symmetry)}</a>
+              </Link>
+            </div>
+          )
+        })}
+      </nav>
+    </div>
+  )
+}
+
+const longName: Record<Symmetry, string> = {
+  all: "Full symmetry",
+  axis2: "Reflective symmetry (2 axes)",
+  diag2: "Diagonal symmetry (2 diagonals)",
+  rot2: "Rotational symmetry (4-fold)",
+  axis: "Reflective symmetry (axis)",
+  diag: "Reflective symmetry (diagonal)",
+  rot: "Rotational symmetry (2-fold)",
+  none: "Asymmetric",
+}
+
+export default function SymmetryInfo({ symmetry, source }: any) {
+  return (
+    <Layout subNav={<SymmetryLinks />}>
+      <main
+        css={css`
+          padding: 2rem 0;
+          max-width: 36rem;
+
+          h1 {
+            margin: 0;
+          }
+        `}
+      >
+        <Link href="/classes">
+          <a>Back</a>
+        </Link>
+        <h1>{(longName as any)[symmetry]}</h1>
+        <MDXRemote {...source} />
+      </main>
+    </Layout>
+  )
+}
+
+export function getStaticPaths() {
+  return {
+    paths: symmetries.map((symmetry) => ({ params: { symmetry } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { symmetry } = params as any
+  const source = fs.readFileSync(
+    `${process.cwd()}/pages/symmetry/subpages/${symmetry}.mdx`,
+    "utf-8",
+  )
+  const mdxSource = await serialize(source)
+  return { props: { symmetry, source: mdxSource } }
+}
