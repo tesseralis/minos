6e47f1b75d4cda43d0850d325657477adad738a1
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-09-04 17:44:22 -0700

    update nextjs

--- next.config.js
+++ next.config.js
@@ -11,8 +11,5 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["page.ts", "page.tsx"],
-  future: {
-    webpack5: true,
-  },
   target: "serverless",
 })
