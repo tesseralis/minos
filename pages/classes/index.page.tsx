c624dbf11a25eef91744b39df4e7d8046d3f47eb
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2023-02-20 18:31:14 -0800

    fix the margin

--- pages/classes/index.page.tsx
+++ pages/classes/index.page.tsx
@@ -112,7 +112,7 @@ export default function ClassesChart() {
     <Layout>
       <main
         css={css`
-          padding: 2rem 0;
+          padding: 2rem;
           display: grid;
           grid-gap: 1.5rem;
           grid-template-columns: 18rem 1fr 1fr 4rem 8rem 10rem;
