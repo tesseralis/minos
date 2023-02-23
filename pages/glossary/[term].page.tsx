24d37bff211a28ce6df37b5df461a3e3b130adc5
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2023-02-22 18:35:07 -0800

    i hate modern web dev

--- pages/glossary/[term].page.tsx
+++ pages/glossary/[term].page.tsx
@@ -1,20 +1,24 @@
-import fs from "fs"
-import remarkMath from "remark-math"
-import rehypeKatex from "rehype-katex"
 import { GetStaticProps } from "next"
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
-import { serialize } from "next-mdx-remote/serialize"
 import { NextPageWithLayout } from "pages/_app.page"
 import { ReactElement } from "react"
 import GlossaryLayout from "./layout"
 import { capitalize } from "lodash"
 import { getTerms } from "./glossaryHelpers"
 
-const Page: NextPageWithLayout<Props> = (({ term, source }: Props) => {
+import fixedPolyomino from "./subpages/fixed-polyomino.mdx"
+import freePolyomino from "./subpages/free-polyomino.mdx"
+
+const pages: Record<string, any> = {
+  "fixed-polyomino": fixedPolyomino,
+  "free-polyomino": freePolyomino,
+}
+
+const Page: NextPageWithLayout<Props> = (({ term }: Props) => {
+  const Text = pages[term]
   return (
     <div>
       <h1>{capitalize(term.replace("-", " "))}</h1>
-      <MDXRemote {...source} />
+      <Text />
     </div>
   )
 }) as any
@@ -28,24 +32,13 @@ export default Page
 interface Props {
   term: string
   terms: string[]
-  source: MDXRemoteSerializeResult
 }
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   // TODO typecheck
   const { term } = params as any
   const terms = getTerms()
-  const source = fs.readFileSync(
-    `${process.cwd()}/pages/glossary/subpages/${term}.mdx`,
-    "utf-8",
-  )
-  const mdxSource = await serialize(source, {
-    mdxOptions: {
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [rehypeKatex],
-    },
-  })
-  return { props: { term, terms, source: mdxSource } }
+  return { props: { term, terms } }
 }
 
 export function getStaticPaths() {
