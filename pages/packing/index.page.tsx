baf83760da3887547366255989cad5ddf85f1e63
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2023-03-11 01:32:29 -0800

    fix packing margins

--- pages/packing/index.page.tsx
+++ pages/packing/index.page.tsx
@@ -5,13 +5,7 @@ import { css } from "@emotion/react"
 export default function PackingPage() {
   return (
     <PackingLayout>
-      <div
-        css={css`
-          margin: 2rem 4rem;
-        `}
-      >
-        <Info />
-      </div>
+      <Info />
     </PackingLayout>
   )
 }
