a1f4c4b591514a38514bed34cf5808e97fbe6835
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2023-03-19 14:28:57 -0700

    scale up mino links on hover

--- components/MinoLink.tsx
+++ components/MinoLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import MinoDiv, { Props as MinoDivProps } from "components/MinoDiv"
+import { css } from "@emotion/react"
 
 interface Props extends Omit<MinoDivProps, "onClick"> {
   to: string
@@ -10,7 +11,17 @@ interface Props extends Omit<MinoDivProps, "onClick"> {
  */
 export default function MinoLink({ to, ...props }: Props) {
   return (
-    <Link href={to}>
+    <Link
+      href={to}
+      css={css`
+        svg {
+          transition: transform 150ms ease-in-out;
+        }
+        svg:hover {
+          transform: scale(1.1);
+        }
+      `}
+    >
       <MinoDiv {...props} />
     </Link>
   )
