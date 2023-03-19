3f16999133fa662c25cc28c567233d5bf25fe0bb
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2023-03-19 02:38:51 -0700

    WIP intro tiles

--- pages/intro/helpers.tsx
+++ pages/intro/helpers.tsx
@@ -3,6 +3,11 @@ import { Polyomino } from "mino"
 import MinoLink from "components/MinoLink"
 import { getMinoColor } from "components/graph"
 import { media } from "style/media"
+import Link from "next/link"
+import { capitalize } from "lodash"
+import { colors } from "style/theme"
+import Tiling from "components/Tiling"
+import MinoPattern from "components/Pattern"
 
 const minos = [
   "1",
@@ -42,4 +47,122 @@ export function MinoList() {
       })}
     </div>
   )
+}
+
+export function LinkCards() {
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-auto-rows: 1fr;
+        gap: 1rem;
+        font-size: 1rem;
+      `}
+    >
+      {cards.map(({ name, description, thumbnail }) => {
+        return (
+          <Link
+            key={name}
+            href={`/${name}`}
+            css={css`
+              border: 1px solid ${colors.border};
+              text-decoration: none;
+              transition: background-color 150ms ease-in-out;
+              :hover {
+                background-color: ${colors.bg2};
+              }
+            `}
+          >
+            <div
+              css={css`
+                height: 8rem;
+                overflow: hidden;
+              `}
+            >
+              {thumbnail}
+            </div>
+            <div
+              css={css`
+                padding: 0.5rem;
+              `}
+            >
+              <div
+                css={css`
+                  color: ${colors.heading};
+                `}
+              >
+                {capitalize(name)}
+              </div>
+              <div>{description}</div>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+const cards = [
+  {
+    name: "catalog",
+    description:
+      "A list of polyominoes up to octominoes with a summary of their properties.",
+  },
+  {
+    name: "symmetry",
+    description:
+      "An explanation of the different types of symmetries a polyomino can have.",
+  },
+  {
+    name: "classes",
+    description:
+      'A hierarchial categorization of polyominoes based on "directedness".',
+  },
+  {
+    name: "packing",
+    description: "Fitting a set of polyominoes tightly in some container.",
+    thumbnail: <PatternThumbnail />,
+  },
+  {
+    name: "Tiling",
+    description: "Whether a single polyomino can completely fill the plane.",
+    thumbnail: <TilingThumbnail />,
+  },
+  {
+    name: "genealogy",
+    description:
+      'A "family tree" showing how polyominoes are built from simpler polyominoes.',
+  },
+  {
+    name: "glossary",
+    description: "A list of polyomino-related terms.",
+  },
+]
+
+function CatalogThumbnail() {
+  return <svg></svg>
+}
+
+// I can't believe this is how I'm actually doing this...
+function PatternThumbnail() {
+  const pattern = `🟥🟩🟩🟧🟧🟧🟧🟨🟨🟨
+🟥🟩🟩🟪🟧🟦🟦🟦🟦🟨
+🟥🟩🟪🟪🟪🟥🟨🟨🟦🟨
+🟥🟦🟧🟪🟥🟥🟥🟨🟪🟪
+🟥🟦🟧🟧🟩🟩🟥🟨🟨🟪
+🟦🟦🟦🟧🟧🟩🟩🟩🟪🟪`
+  return (
+    <div
+      css={css`
+        pointer-events: none;
+      `}
+    >
+      <MinoPattern pattern={pattern} />
+    </div>
+  )
+}
+
+function TilingThumbnail() {
+  return <Tiling mino={Polyomino.of("00011_10110_11100")} gridSize={12} />
 }
