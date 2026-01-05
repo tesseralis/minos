3bb0e5e3081e9d93ef81d8f4f118833f5e1e4472
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-01-05 05:05:05 -0800

    Add breadcrumbs to glossary.

--- src/routes/(nav)/glossary/[term]/+page.svelte
+++ src/routes/(nav)/glossary/[term]/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+  import Breadcrumbs from "$lib/components/Breadcrumbs.svelte"
+  import { page } from "$app/state"
   const { data } = $props()
   const Content = $derived(data.content)
 </script>
 
+<Breadcrumbs
+  paths={[
+    ["Glossary", "/glossary"],
+    [data.title, `/glossary/${page.params.term}`],
+  ]}
+/>
 <h1>{data.title}</h1>
 <Content />
