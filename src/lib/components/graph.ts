e5ea851c45a1021090f56c1cba06b736e8e01329
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-01-26 23:25:41 -0800

    Add the graph back in

--- src/lib/components/graph.ts
+++ src/lib/components/graph.ts
@@ -7,6 +7,7 @@ import {
   type Symmetry,
   MONOMINO,
 } from "$lib/mino"
+import { generateGraph } from "$lib/mino/enumerate"
 
 type Color = tinycolor.Instance
 
@@ -72,42 +73,6 @@ function sortGeneration(minos: Polyomino[]) {
     (mino) => -Math.max(...mino.dims),
     (mino) => -Math.min(...mino.dims),
   )
-}
-
-export function generateGraph(
-  n: number,
-  sort?: (minos: Polyomino[]) => Polyomino[],
-) {
-  const nodes: Polyomino[][] = []
-  const links: [Polyomino, Polyomino][] = []
-
-  // mapping from each mino to its index in the generation
-  const indices = new Map<Polyomino, number>()
-  const visited = new Set<Polyomino>([MONOMINO])
-  let currentGen = [MONOMINO]
-
-  while (nodes.length < n - 1) {
-    const nextGen = []
-    for (const mino of currentGen) {
-      for (const child of mino.freeChildren()) {
-        if (!visited.has(child)) {
-          nextGen.push(child)
-          visited.add(child)
-        }
-        links.push([mino, child])
-      }
-    }
-
-    nodes.push(currentGen)
-    // currentGen = nextGen
-    currentGen = sort?.(nextGen) ?? nextGen
-    currentGen.forEach((mino, i) => {
-      indices.set(mino, i)
-    })
-  }
-  nodes.push(currentGen)
-
-  return { nodes, links, indices }
 }
 
 export const NUM_GENERATIONS = 8
