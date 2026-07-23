f217f77baedf6780437b9116661aec28ffa64771
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-07-22 21:45:18 -0700

    Add description tag for SEO for some reason

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,10 @@
 
 <svelte:head>
   <title>The Labyrinth of Polyominoes</title>
+  <meta
+    name="description"
+    content="Interactive encyclopedia and shrine to polyominoes."
+  />
 </svelte:head>
 <Tooltip.Provider>
   {@render children()}
