81b79aefe71e0e29dbdad6010313b10ca302cbdd
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-07-29 20:15:09 -0700

    Write more and move directory

---
+++ src/routes/(nav)/articles/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const { children } = $props()
+</script>
+
+<div class="wrapper">
+  {@render children()}
+</div>
+
+<style>
+  .wrapper {
+    padding: 2rem 4rem;
+    max-width: 72rem;
+  }
+</style>
