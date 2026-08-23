ab3f47c273dad092904ba1c48bdb29376e1c3a0b
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-08-23 01:32:46 -0700

    Move generation to its own file; rename the test file.

---
+++ mino/src/bin/test.rs
@@ -0,0 +1,17 @@
+use mino::enumerate::enumerate_minos;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let n: usize = args[1].parse().expect("Requires an integer");
+
+    let nodes = enumerate_minos(n);
+    for gen in nodes {
+        println!("{:?}", gen.len());
+        println!("");
+        // let counts = gen.into_iter().counts_by(|mino| mino.get_dir_class());
+        // for (cls, count) in counts.iter() {
+        //     println!("{:?}: {}", cls, count);
+        // }
+    }
+}
