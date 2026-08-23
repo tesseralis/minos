fee3d9f8d3b8939a4cdc05f8471b858ec506c368
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-08-23 14:39:05 -0700

    Clean up enumerate function.

--- mino/src/enumerate.rs
+++ mino/src/enumerate.rs
@@ -3,6 +3,16 @@ use rustc_hash::FxBuildHasher;
 
 use crate::{mino::Polyomino, point::Point, transform::Transformable};
 
+fn generate_children(minos: &Vec<Polyomino>) -> Vec<Polyomino> {
+    minos
+        .iter()
+        .flat_map(|mino| mino.children())
+        .unique_with_hasher(FxBuildHasher)
+        .map(|child| child.free())
+        .unique_with_hasher(FxBuildHasher)
+        .collect()
+}
+
 // TODO Ideally this would be an infinite iterator but I was having trouble getting the ownership to work
 pub fn enumerate_minos(n: usize) -> Vec<Vec<Polyomino>> {
     let monomino: Polyomino = Polyomino::new(vec![Point::new(0, 0)]);
@@ -12,13 +22,7 @@ pub fn enumerate_minos(n: usize) -> Vec<Vec<Polyomino>> {
     let mut current_gen = vec![monomino];
 
     while gens.len() < n - 1 {
-        let next_gen = current_gen
-            .iter()
-            .flat_map(|mino| mino.children())
-            .unique_with_hasher(FxBuildHasher)
-            .map(|child| child.free())
-            .unique_with_hasher(FxBuildHasher)
-            .collect();
+        let next_gen = generate_children(&current_gen);
         gens.push(current_gen);
         current_gen = next_gen;
     }
