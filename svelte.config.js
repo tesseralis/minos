ff0f787e013f7c56e04927718efc67c1e5262bfb
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-09-10 21:26:29 -0700

    Make footnotes work.

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,18 @@ import adapter from "@sveltejs/adapter-netlify"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 import rehypeKatexSvelte from "rehype-katex-svelte"
 import remarkMath from "remark-math"
+import remarkFootnotes from "remark-footnotes"
 
 const config = {
   preprocess: [
     vitePreprocess(),
     mdsvex({
       extensions: [".md", ".svx"],
-      remarkPlugins: [[remarkMath, { strict: false }]],
+      remarkPlugins: [
+        // this is a deprecated module, but required: https://github.com/pngwn/MDsveX/discussions/223#discussioncomment-541337
+        remarkFootnotes,
+        [remarkMath, { strict: false }],
+      ],
       rehypePlugins: [[rehypeKatexSvelte, { strict: false }]],
     }),
   ],
