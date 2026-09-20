b6c76fee2345ed117fdb662e64e308ff702eb8d4
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2026-09-20 00:34:41 -0700

    Do some rearrangement and style some things.

--- src/routes/(nav)/articles/+layout.svelte
+++ src/routes/(nav)/articles/+layout.svelte
@@ -17,9 +17,24 @@
         <li><a href="directedness-1">Directedness 1</a></li>
         <li><a href="directedness-2">Directedness 2</a></li>
         <li><a href="directedness-3">Directedness 3</a></li>
-        <li><a href="directedness-4">Directedness 4</a></li>
       </ul>
     </div>
   {/snippet}
-  {@render children()}
+  <article>
+    {@render children()}
+  </article>
 </NavAndContent>
+
+<style>
+  article {
+    max-width: 1000px;
+  }
+
+  article :global(blockquote) {
+    font-style: italic;
+  }
+
+  article :global(img) {
+    max-width: 800px;
+  }
+</style>
